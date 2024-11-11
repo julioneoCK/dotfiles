1120602e746805064fc7cf3f1f5944d037dacaee
julioneoCK <julio.macias@neock.es>
2024-11-11 09:51:32 +0100

    feat: remove noice and notify

--- .config/nvim/lua/julio/plugins/notify.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"rcarriga/nvim-notify",
-	opts = {
-		render = "compact", -- Use compact rendering for notifications
-		background_colour = "#1a1b26", -- Set the background color for notifications
-		timeout = 1000, -- Set the timeout for notifications (in milliseconds)
-	},
-}
